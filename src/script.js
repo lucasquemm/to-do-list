ff239c93f05b36edb9625c636a7235ddb06292bf
Lucas Brasil <lucasbcmoraes@gmail.com>
2021-08-18 20:01:02 -0300

    implementacao da fila de criacao/exibicao de projetos

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import { Tarefa } from './tarefa'
 
 let listaTarefas = []
+let listaProjetos = []
 
 const telaLista = document.querySelector('.painel-lista')
 const form = document.querySelector('#formTarefa')
@@ -9,16 +10,28 @@ const recebeTitulo = document.querySelector('#recebeTitulo')
 const recebeDescricao = document.querySelector('#recebeDescricao')
 const recebePrioridade = document.querySelector('#recebePrioridade')
 
+const mudaModoDeSalvar = document.querySelector('#troca-lista')
+
+let modo = false
+
 const salvaTarefa = (titulo, descricao, prioridade) => {
-  listaTarefas.push(new Tarefa(titulo, descricao, prioridade))
+  if (!modo) {
+    listaTarefas.push(new Tarefa(titulo, descricao, prioridade))
+  } else {
+    listaProjetos.push(new Tarefa(titulo, descricao, prioridade))
+  }
 }
 
 const mostraTarefa = () => {
   telaLista.innerHTML = ''
-  listaTarefas.forEach((tarefa) => criaTarefaItem(tarefa))
+  if (!modo) {
+    listaTarefas.forEach((tarefa) => criaItem(tarefa))
+  } else {
+    listaProjetos.forEach((projeto) => criaItem(projeto))
+  }
 }
 
-const criaTarefaItem = (tarefa) => {
+const criaItem = (tarefa) => {
   const itemTarefa = document.createElement('div')
   itemTarefa.classList.add('item-tarefa')
 
@@ -41,6 +54,12 @@ const criaTarefaItem = (tarefa) => {
     itemTarefa.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
   }
 
+  if (!modo) {
+    itemTarefa.style.borderRadius = '8px'
+  } else {
+    itemTarefa.style.borderRadius = '0px'
+  }
+
   telaLista.appendChild(itemTarefa)
   itemTarefa.appendChild(tituloItem)
   itemTarefa.appendChild(descricaoItem)
@@ -57,4 +76,13 @@ form.addEventListener('submit', function (e) {
   e.preventDefault()
   salvaTarefa(recebeTitulo.value, recebeDescricao.value, recebePrioridade.value)
   mostraTarefa()
+})
+
+mudaModoDeSalvar.addEventListener('click', function () {
+  modo = !modo
+  if (modo) {
+    mudaModoDeSalvar.textContent = 'Mudar Para Tarefas'
+  } else {
+    mudaModoDeSalvar.textContent = 'Mudar Para Projetos'
+  }
 })
