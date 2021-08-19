cc61bea99b57b5b729499aee6b8f5e1c10345c47
Lucas Brasil <lucasbcmoraes@gmail.com>
2021-08-18 22:47:07 -0300

    adicao de botao para mudar o modo de cadastro de tarefa

--- src/script.js
+++ src/script.js
@@ -5,13 +5,11 @@ let listaTarefas = []
 let listaProjetos = []
 
 const telaLista = document.querySelector('.painel-lista')
-const formTarefa = document.querySelector('#formTarefa')
+const formTarefa = document.querySelector('#form-tarefa')
 const formProjeto = document.querySelector('#form-projeto')
-
 const recebeTituloTarefa = document.querySelector('#recebeTitulo')
 const recebeDescricaoTarefa = document.querySelector('#recebeDescricao')
 const recebePrioridadeTarefa = document.querySelector('#recebePrioridade')
-
 const recebeTituloProjeto = document.querySelector('#recebe-titulo-projeto')
 const recebeDescricaoProjeto = document.querySelector(
   '#recebe-descricao-projeto',
@@ -19,7 +17,6 @@ const recebeDescricaoProjeto = document.querySelector(
 const recebePrioridadeProjeto = document.querySelector(
   '#recebe-prioridade-projeto',
 )
-
 const mudaModoDeSalvar = document.querySelector('#troca-lista')
 
 let modoProjeto = false
@@ -32,43 +29,6 @@ const mostraTarefa = () => {
   telaLista.innerHTML = ''
   listaTarefas.forEach((tarefa) => criaTarefa(tarefa))
 }
-
-const criaTarefa = (tarefa) => {
-  const itemTarefa = document.createElement('div')
-  itemTarefa.classList.add('item-tarefa')
-
-  const tituloItem = document.createElement('h2')
-  tituloItem.classList.add('titulo-item-tarefa')
-  tituloItem.textContent = `${tarefa.titulo}`
-
-  const descricaoItem = document.createElement('p')
-  descricaoItem.classList.add('descricao-item-tarefa')
-  descricaoItem.textContent = `${tarefa.descricao}`
-
-  const prioridadeItem = document.createElement('p')
-  prioridadeItem.classList.add('prioridade-item-tarefa')
-
-  if (`${tarefa.prioridade}` == 3) {
-    itemTarefa.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
-  } else if (tarefa.prioridade == 2) {
-    itemTarefa.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
-  } else {
-    itemTarefa.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
-  }
-
-  if (!modoProjeto) {
-    itemTarefa.style.borderRadius = '8px'
-  } else {
-    itemTarefa.style.borderRadius = '0px'
-  }
-
-  telaLista.appendChild(itemTarefa)
-  itemTarefa.appendChild(tituloItem)
-  itemTarefa.appendChild(descricaoItem)
-  itemTarefa.appendChild(prioridadeItem)
-}
-
-//------------
 
 const salvaProjeto = (titulo, descricao, prioridade) => {
   listaProjetos.push(new Projeto(titulo, descricao, prioridade))
@@ -110,6 +70,34 @@ const criaProjeto = (projeto) => {
   itemProjeto.appendChild(prioridadeItem)
 }
 
+const criaTarefa = (tarefa) => {
+  const itemTarefa = document.createElement('div')
+  itemTarefa.classList.add('item-tarefa')
+
+  const tituloItem = document.createElement('h2')
+  tituloItem.classList.add('titulo-item-tarefa')
+  tituloItem.textContent = `${tarefa.titulo}`
+
+  const descricaoItem = document.createElement('p')
+  descricaoItem.classList.add('descricao-item-tarefa')
+  descricaoItem.textContent = `${tarefa.descricao}`
+
+  const prioridadeItem = document.createElement('p')
+  prioridadeItem.classList.add('prioridade-item-tarefa')
+
+  if (`${tarefa.prioridade}` == 3) {
+    itemTarefa.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
+  } else if (tarefa.prioridade == 2) {
+    itemTarefa.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
+  } else {
+    itemTarefa.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
+  }
+
+  telaLista.appendChild(itemTarefa)
+  itemTarefa.appendChild(tituloItem)
+  itemTarefa.appendChild(descricaoItem)
+  itemTarefa.appendChild(prioridadeItem)
+}
 const mudaExibicao = () => {
   if (modoProjeto) {
     return (mudaModoDeSalvar.textContent = 'Mudar Para Tarefas')
@@ -118,7 +106,15 @@ const mudaExibicao = () => {
   }
 }
 
-const trocaModo = () => {}
+const trocaModo = () => {
+  if (modoProjeto) {
+    formTarefa.classList.add('hidden')
+    formProjeto.classList.remove('hidden')
+  } else {
+    formProjeto.classList.add('hidden')
+    formTarefa.classList.remove('hidden')
+  }
+}
 
 const html = (str) => {
   const el = document.createElement('div')
@@ -134,6 +130,7 @@ formTarefa.addEventListener('submit', function (e) {
     recebePrioridadeTarefa.value,
   )
   mostraTarefa()
+  console.log(listaTarefas)
 })
 
 formProjeto.addEventListener('submit', function (e) {
@@ -149,4 +146,5 @@ formProjeto.addEventListener('submit', function (e) {
 mudaModoDeSalvar.addEventListener('click', function () {
   modoProjeto = !modoProjeto
   mudaExibicao()
+  trocaModo()
 })
