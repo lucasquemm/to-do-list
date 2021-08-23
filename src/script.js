e6d946f06529ab4a5b5256f11bc60777833c3401
Lucas Brasil <lucasbcmoraes@gmail.com>
2021-08-23 20:27:31 -0300

    alteracao na funcao de cadastro e exibicao dos projetos/tarefas

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import { Tarefa } from './tarefa'
 import { Projeto } from './projeto'
+import './style.css'
 
 let listaTarefas = []
 let listaProjetos = []
@@ -43,61 +44,80 @@ const criaProjeto = (projeto) => {
   const itemProjeto = document.createElement('div')
   itemProjeto.classList.add('item-projeto')
 
+  const itemCorpo = document.createElement('div')
+  itemCorpo.classList.add('corpo-item-projeto')
+
+  const itemMod = document.createElement('div')
+  itemMod.classList.add('mod-item-projeto')
+
   const tituloItem = document.createElement('h2')
-  tituloItem.classList.add('titulo-item-projeto')
+  tituloItem.setAttribute('id', 'titulo-item-projeto')
   tituloItem.textContent = `${projeto.titulo}`
 
   const descricaoItem = document.createElement('p')
-  descricaoItem.classList.add('descricao-item-projeto')
+  descricaoItem.setAttribute('id', 'descricao-item-projeto')
   descricaoItem.textContent = `${projeto.descricao}`
 
-  const prioridadeItem = document.createElement('p')
-  prioridadeItem.classList.add('prioridade-item-projeto')
+  const prioridadeItem = document.createElement('button')
+  prioridadeItem.setAttribute('id', 'prioridade-item-projeto')
+  prioridadeItem.textContent = '*'
 
   if (`${projeto.prioridade}` == 3) {
-    itemProjeto.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
   } else if (projeto.prioridade == 2) {
-    itemProjeto.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
   } else {
-    itemProjeto.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
   }
 
   itemProjeto.style.borderRadius = '0px'
 
   telaLista.appendChild(itemProjeto)
-  itemProjeto.appendChild(tituloItem)
-  itemProjeto.appendChild(descricaoItem)
-  itemProjeto.appendChild(prioridadeItem)
+  itemProjeto.appendChild(itemCorpo)
+  itemProjeto.appendChild(itemMod)
+  itemCorpo.appendChild(tituloItem)
+  itemCorpo.appendChild(descricaoItem)
+  itemMod.appendChild(prioridadeItem)
 }
 
 const criaTarefa = (tarefa) => {
   const itemTarefa = document.createElement('div')
   itemTarefa.classList.add('item-tarefa')
 
+  const itemCorpo = document.createElement('div')
+  itemCorpo.classList.add('corpo-item-tarefa')
+
+  const itemMod = document.createElement('div')
+  itemMod.classList.add('mod-item-tarefa')
+
   const tituloItem = document.createElement('h2')
-  tituloItem.classList.add('titulo-item-tarefa')
+  tituloItem.setAttribute('id', 'titulo-item-tarefa')
   tituloItem.textContent = `${tarefa.titulo}`
 
   const descricaoItem = document.createElement('p')
-  descricaoItem.classList.add('descricao-item-tarefa')
+  descricaoItem.setAttribute('id', 'descricao-item-tarefa')
   descricaoItem.textContent = `${tarefa.descricao}`
 
-  const prioridadeItem = document.createElement('p')
-  prioridadeItem.classList.add('prioridade-item-tarefa')
+  const prioridadeItem = document.createElement('button')
+  prioridadeItem.setAttribute('id', 'prioridade-item-tarefa')
+  prioridadeItem.textContent = '*'
 
   if (`${tarefa.prioridade}` == 3) {
-    itemTarefa.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(238, 4, 4, 0.900)'
   } else if (tarefa.prioridade == 2) {
-    itemTarefa.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(253, 173, 52, 0.900)'
   } else {
-    itemTarefa.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
+    prioridadeItem.style.backgroundColor = 'rgba(10, 126, 0, 0.900)'
   }
 
   telaLista.appendChild(itemTarefa)
-  itemTarefa.appendChild(tituloItem)
-  itemTarefa.appendChild(descricaoItem)
-  itemTarefa.appendChild(prioridadeItem)
+  itemTarefa.appendChild(itemCorpo)
+  itemTarefa.appendChild(itemMod)
+  itemCorpo.appendChild(tituloItem)
+  itemCorpo.appendChild(descricaoItem)
+  itemMod.appendChild(prioridadeItem)
 }
+
 const mudaExibicao = () => {
   if (modoProjeto) {
     return (mudaModoDeSalvar.textContent = 'Mudar Para Tarefas')
@@ -116,11 +136,11 @@ const trocaModo = () => {
   }
 }
 
-const html = (str) => {
-  const el = document.createElement('div')
-  el.innerHTML = str
-  return el.firstChild
-}
+// const html = (str) => {
+//   const el = document.createElement('div')
+//   el.innerHTML = str
+//   return el.firstChild
+// }
 
 formTarefa.addEventListener('submit', function (e) {
   e.preventDefault()
